1e8af9d9a1f4e6bf4b8eaf54bcbf0897f68238f9
bprudencio-dev <bprudencio.dev@gmail.com>
2023-08-02 12:11:19 +0100

    Fresh start

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar/NavBar";
-import HomePage from "./pages/homepage/HomePage";
-import Search from "./pages/search/Search";
-import About from "./pages/about/About";
-
-import RegisterForm from "./pages/Register Form/Register";
-
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/register" element={<RegisterForm />} />
-      </Routes>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
