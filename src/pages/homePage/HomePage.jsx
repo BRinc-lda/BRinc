87d8bd57b19d79e2d9c6581b6ee4531ab1c9c5e1
bprudencio-dev <bprudencio.dev@gmail.com>
2023-08-08 17:26:21 +0100

    homepage fixed and other minor issues

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,17 +1,20 @@
-import BookList from "../../components/BookList/BookList";
 import NavBar from "../../components/NavBar/NavBar";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import FavoriteBooks from "../../components/FavoriteBooks/FavoriteBooks";
 function HomePage() {
   return (
     <div className="homepage">
       <NavBar />
-      <BookList />
+      <h2 className="brincLibrary">
+        BRinc<span>Library</span>
+      </h2>
       <div className="btncontainer">
         <Link to="register">
           <button className="regbutton">Register Now</button>
         </Link>
       </div>
+      <FavoriteBooks />
     </div>
   );
 }
