ae9af8d7bef46fd87e4d7d2f9a9fc5f0d8a5433c
Reuben <treuben90@yahoo.com>
2023-08-08 19:39:05 +0300

    styled the book info pages and worked on the responsiveness of the search pages

--- src/Pages/payment page/PaymentDone.jsx
+++ src/Pages/payment page/PaymentDone.jsx
@@ -9,6 +9,7 @@ function PaymentDone() {
       <NavBar />
       <div className="vectContent">
         <img src={vector} alt="A payment vector" className="vector" />
+        <p>Thank you for Purchasing from BRinc</p>
         <Link to="/search">
           <button className="btnVector">Done</button>
         </Link>
